6beac81804ca340cec1b784241ffe0105fbb64a5
Mohammed-Aljablai <mohammed.aljablai@gmail.com>
2025-04-12 12:06:27 +0300

    create controllers

---
+++ Controllers/books_controller.php
@@ -0,0 +1,85 @@
+<?php
+  include_once "models/model_book.php";
+  class BookController {
+    private $bookDB;
+
+    // todo: Security & better & Error
+    function __construct() {
+      $this->bookDB = new BookDB("book_club", "root");
+    }
+
+    // todo: Upload A book
+    function addBook($data){
+      $data+=["author_id" => 0];
+      // $inputs_array["book_description"] = ($inputs_array["book_description"]) ? $inputs_array["book_description"] : null;
+      // echo  ($inputs_array["book_description"]) ? "true" : "false" ;
+      return $this->bookDB->uploadBook($data);
+    }
+
+    function getBooks(){
+      return $this->bookDB->getBooks();
+    }
+
+    function searchForBooks($book){
+      // Todo: Filter the search input
+      return $this->bookDB->searchForBooks($book);
+    }
+
+    function uploadFile($input_name, $type, $maxSizeInMB = 100) : string {
+      if (!isset($_FILES[$input_name])) // Check if the file exist
+        throw new Exception("There is no file to upload.");
+      if($_FILES[$input_name]['error']!=0)
+        throw new Exception("the file dose not uploaded correctly");
+        
+      // Having file info
+      $filepath = $_FILES[$input_name]['tmp_name'];
+      $fileSize = filesize($filepath);
+
+      // todo: understand this 2 lines
+      $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+      $filetype = finfo_file($fileinfo, $filepath);
+
+      if ($fileSize === 0) // If the File empty
+        throw new Exception("The file is empty.");
+
+      if ($fileSize > (1024 * 1024 * $maxSizeInMB)) // 3 MB (1 byte * 1024 * 1024 * 3 (for 3 MB))
+        throw new Exception("The file is too large");
+      
+      if ($type = "pdfs")
+        $allowedTypes = [ 'application/pdf' => 'pdf'];
+      elseif($type = "imgs")
+        $allowedTypes = [ 'image/png' => 'png', 'image/jpeg' => 'jpg' ];
+      else
+        $allowedTypes = [];
+      
+      if (!in_array($filetype, array_keys($allowedTypes))) // only allowed Typed
+        throw new Exception("File not allowed.");
+      
+      // make the new path and file
+      $filename = uniqid("file_");
+      $extension = $allowedTypes[$filetype];
+      $targetDirectory = dirname(__DIR__) . "/uploads/$type"; // dirname(__DIR__) is the parent directory of the current PHP file  
+      $newFilepath = $targetDirectory . "/" . $filename . "." . $extension;
+    
+      if (!copy($filepath, $newFilepath)) // Copy the file, returns false if failed
+        throw new Exception("Can't move file.");
+      unlink($filepath); // Delete the temp file
+      
+      return "uploads/$type/$filename.$extension";
+    }
+
+    // function getBook($book_name, $book_author){
+    //   // todo: Filter
+    //   return $this->bookDB->getBook($book_name, $book_author);
+    // }
+  }
+
+  // $myBooks = new BookController();
+  // print_r( $myBooks->getBooks() );
+
+  // $bookDB = new BookDB("book_club", "root");
+  // echo "<pre>";
+  // print_r($bookDB->getBooks());
+  // print_r($bookDB->searchForBooks("unknown"));
+  // print_r($bookDB->getBook("Database deisign", "'unknown'"));
+  // echo "</pre>";
--- Controllers/redirect_to_login.php
+++ Controllers/redirect_to_login.php
@@ -8,7 +8,7 @@
       isset($_COOKIE['user_email']) &&
       $check_db->checkUser($_COOKIE['remember_me'], $_COOKIE['user_email']))
     ) {
-      header("location: index.php");
+      header("location: login.php");
     }else{
       setcookie("user_fullname", $_COOKIE['user_fullname'], ['expires' => time() + (30 * 24 * 60 * 60)]);
       setcookie("user_email", $_COOKIE['user_email'], ['expires' => time() + (30 * 24 * 60 * 60)]);
